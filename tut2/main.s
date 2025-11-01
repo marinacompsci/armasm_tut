f4648ed4373d36bb8c52664244bb38fb75ecb7b6
MBSA <marinacompsci@protonmail.com>
2025-11-01 11:16:48 +0100

    fix: remove exit value typo

--- tut2/main.s
+++ tut2/main.s
@@ -10,7 +10,7 @@ _start:
     svc #0
 
 
-    mov x0, #1              // arg0: int - 1(success)
+    mov x0, #0              // arg0: int - 0(success)
     mov x8, #93             // syscall: exit
     svc #0
 
