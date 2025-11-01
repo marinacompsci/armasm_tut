c20b99083ba544766388d5d16295a62f7bfeb469
MBSA <marinacompsci@protonmail.com>
2025-11-01 11:04:16 +0100

    feat: write to stdout

---
+++ tut2/main.s
@@ -0,0 +1,18 @@
+.global _start
+
+
+
+_start:
+    mov x0, #1              // arg0: fd - stdout(0x01)
+    ldr x1, =msg            // arg1: *buf - msg
+    mov x2, #14             // arg2: strlen
+    mov x8, #64             // syscall: write
+    svc #0
+
+
+    mov x0, #1              // arg0: int - 1(success)
+    mov x8, #93             // syscall: exit
+    svc #0
+
+.data
+    msg: .ascii "hakuna matata\n"
